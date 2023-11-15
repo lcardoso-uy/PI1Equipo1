f2aa57f8ecc15468e3261e2401e5977cd5e950ee
lcardoso-uy <lcardosogx@gmail.com>
2023-11-15 20:39:09 -0300

    [INTEGRACION] Ejemplo login

--- integracion/naturepic/src/components/Login.jsx
+++ integracion/naturepic/src/components/Login.jsx
@@ -5,18 +5,16 @@ class Login extends React.Component {
         super(props);
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            userInfo: null,
+            loginError: null,
+            userInfoError: null
         };
     }
 
     handleInputChange = (event) => {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
     }
 
     handleSubmit = async (event) => {
@@ -24,52 +22,76 @@ class Login extends React.Component {
         try {
             const response = await fetch('http://localhost:8080/auth/generateToken', {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username: this.state.username,
-                    password: this.state.password
-                }),
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({ username: this.state.username, password: this.state.password }),
             });
-    
+
             if (response.ok) {
                 const token = await response.text();
-                console.log('Login successful, token:', token);
                 localStorage.setItem('token', token);
+                this.fetchUserInfo(token);
             } else {
-                console.error('Login failed');
+                this.setState({ loginError: 'Login failed' });
             }
         } catch (error) {
-            console.error('Error during login:', error);
+            this.setState({ loginError: 'Error during login' });
         }
     }
-        
+
+    fetchUserInfo = async (token) => {
+        try {
+            const response = await fetch('http://localhost:8080/auth/getUserInfo', {
+                method: 'GET',
+                headers: {'Authorization': `Bearer ${token}`}
+            });
+
+            if (response.ok) {
+                const userInfo = await response.json();
+                this.setState({ userInfo });
+            } else {
+                this.setState({ userInfoError: 'Failed to load user info' });
+            }
+        } catch (error) {
+            this.setState({ userInfoError: 'Error fetching user info' });
+        }
+    }
+
     render() {
+        const { username, password, userInfo, loginError, userInfoError } = this.state;
+
+        if (userInfo) {
+            return (
+                <div>
+                    <h1>User Profile</h1>
+                    <p>Username: {userInfo.username}</p>
+                    <p>Firstname: {userInfo.firstname}</p>
+                    <p>Surname: {userInfo.surname}</p>
+                    <p>Email: {userInfo.email}</p>
+                    <p>Roles: {userInfo.roles}</p>
+                    <p>Is Admin: {userInfo.isAdmin ? 'Yes' : 'No'}</p>
+                </div>
+            );
+        }
+
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
                     Username:
-                    <input
-                        name="username"
-                        type="email"
-                        value={this.state.username}
-                        onChange={this.handleInputChange} />
+                    <input name="username" type="email" value={username} onChange={this.handleInputChange} />
                 </label>
                 <br />
                 <label>
                     Password:
-                    <input
-                        name="password"
-                        type="password"
-                        value={this.state.password}
-                        onChange={this.handleInputChange} />
+                    <input name="password" type="password" value={password} onChange={this.handleInputChange} />
                 </label>
                 <br />
                 <button type="submit">Login</button>
+                {loginError && <p>{loginError}</p>}
+                {userInfoError && <p>{userInfoError}</p>}
             </form>
         );
     }
 }
 
 export default Login;
+
