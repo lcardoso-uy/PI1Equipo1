c56d871bfb323d4bb43447e8685f8d8d124d77c1
lcardoso-uy <lcardosogx@gmail.com>
2023-11-20 07:07:02 -0300

    [BACKEND][DEV][18_1] Agrego CORS a calendario

--- backend/NaturePic/src/main/java/com/naturepic/home/controller/AuthProductCalendarController.java
+++ backend/NaturePic/src/main/java/com/naturepic/home/controller/AuthProductCalendarController.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/product-calendar")
 public class AuthProductCalendarController {
